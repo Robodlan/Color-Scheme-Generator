8ad613855d47e6cf4d15ed1d2c8b9df0cd8fb9ef
Robodlan <Robodlan@users.noreply.github.com>
2022-11-04 22:00:14 -0400

    clipboard working

--- index.js
+++ index.js
@@ -1,72 +1,66 @@
 const colorInput = document.getElementById('color-input')
+const body = document.getElementById('body')
 const mode = document.getElementById('mode-input')
 const btn = document.getElementById('btn')
 const grid = document.getElementById('grid')
 const hexValue = document.getElementById('hex-value')  
-const copied = document.getElementById('copied')
-var clipboard = new ClipboardJS('.text')  
+// const copied = 
+var clipboard = new ClipboardJS('#text')  
 
-btn.addEventListener('click', getColor)
+
+
+btn.addEventListener('click', (e)=> {
+   e.preventDefault()
+   getColor()
+})
 
 function getColor() {
-  
-     fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${mode.value}`)
+   fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${mode.value}`)
         .then(res => res.json())
         .then(data => {
             const postColors = data.colors
             render(postColors)
+            
             copied.style.display = "none"
         })
 }
-
 
 function render(postColors) {
     let html = ''
     for (let color of postColors){
        html += `
          <div class="grid" style="background-color:${color.hex.value}">
-           <h2 class="text " data-clipboard-text="${color.hex.value}"><span>${color.hex.value}</span></h2> 
+           <h3 id="text" class="text" onclick="clicked()" data-clipboard-text="${color.hex.value}"><span>${color.hex.value}</span></h3> 
          </div>
         `}
+        
     grid.innerHTML = html
+}
+
+function clicked() {
+  document.getElementById('copied').style.color = 'red'; 
 }
 
 clipboard.on('success', function (e) {
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
-    copied.color = 'red'
- });
+  });
 
- 
-
-
-  clipboard.on('error', function (e) {
+clipboard.on('error', function (e) {
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
   });
 
-// function copyNames(postColors) {
-//     let copyText = ""
-//     let text = color.hex.value
-//     for (let names of postColors) {
-//      copyText += 
-//         names.addEventListener('click', ()=> {
-        
-//         navigator.clipboard.writeText(`${text}`)
-//       })       
-//     }
 
-//     grid.innerHTML =  copyText
-// }
 
 function randomColor() {
-    let result = ''
-    const combination = "abcdefghijklm123456789";
-    const combLength = combination.length
-     for (let i = 0; i < 6; i++){
-      result += combination.charAt(Math.floor(Math.random() * combLength[i]))
+    const combination = "abcdefg0123456789";
+    let result = '';
+    const combLength = combination.length;
+    for (let i = 0; i < 6; i++){
+      result += combination.charAt(Math.floor(Math.random() * combLength))
      }
     return result
   }
@@ -75,8 +69,10 @@ function randomColor() {
   function manyColors() {
     const colores  = randomColor();
     colorInput.defaultValue = '#' + colores
+    body.style.backgroundColor = '#' + colores
     getColor();
   }
-  manyColors()
+
+ manyColors()
 
 
