628d6bacf501fe551fcb71853d193532126dda0d
Robodlan <reinaldo.robles@icloud.com>
2024-02-06 09:12:48 -0400

    Clipboard

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ const grid = document.getElementById('grid');
 const hexValue = document.getElementById('hex-value');
 const copy = document.getElementById('copied');  
 
-var clipboard = new ClipboardJS('#text')  
+
+const clipboard = new ClipboardJS('#text')  
 
 
 btn.addEventListener('click', (e)=> {
    e.preventDefault()
    getColor()
-   
 })
-
-
 
 function getColor() {
    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${mode.value}`)
@@ -24,8 +22,7 @@ function getColor() {
             const postColors = data.colors
             render(postColors)
         })
-        
-}
+  }
 
 function render(postColors) {
     let html = ''
@@ -35,23 +32,23 @@ function render(postColors) {
            <h3 id="text" class="text" onclick="clicked()" data-clipboard-text="${color.hex.value}"><span>${color.hex.value}</span></h3> 
          </div>
         `}
-        
-    grid.innerHTML = html
+   grid.innerHTML = html
 }
 
 clipboard.on('success', function (e) {
-    copy.textContent = (`${e.action} ${e.text}`)
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
+  // text.innerHTML = (`${e.action} ${e.text}`)
+    copy.innerHTML = (`${e.action} ${e.text}`)
+    // console.info('Action:', e.action);
+    // console.info('Text:', e.text);
+    // console.info('Trigger:', e.trigger);
     setTimeout(copy.textContent = "", 6000);
   });
 
-clipboard.on('error', function (e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-  });
+// clipboard.on('error', function (e) {
+//     console.info('Action:', e.action);
+//     console.info('Text:', e.text);
+//     console.info('Trigger:', e.trigger);
+//   });
 
 function randomColor() {
     const combination = "abcdefg0123456789";
@@ -60,15 +57,15 @@ function randomColor() {
     for (let i = 0; i < 6; i++){
       result += combination.charAt(Math.floor(Math.random() * combLength))
      }
-    return result
-  }
+  return result
+}
   
 function manyColors() {
     const colores  = randomColor();
     colorInput.defaultValue = '#' + colores
     body.style.backgroundColor = '#' + colores
     getColor();
-  }
+}
 
  manyColors()
  const timeOut = () => {
