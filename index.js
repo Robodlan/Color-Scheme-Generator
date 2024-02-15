12cde735f3d3791ff33d7ca7bed23c20324b66a3
Robodlan <reinaldo.robles@icloud.com>
2024-02-14 20:34:45 -0400

    Copy to cli[board

--- index.js
+++ index.js
@@ -5,76 +5,82 @@ const mode = document.getElementById('mode-input');
 const btn = document.getElementById('btn');
 const grid = document.getElementById('grid');
 const hexValue = document.getElementById('hex-value');
-const copy = document.getElementById('copied');  
+const copy = document.getElementById('copied');
 const text = document.getElementById("text");
+const title = document.getElementById("title");
 
 
-const clipboard = new ClipboardJS('.copied')  
+const clipboard = new ClipboardJS('#text')
 
 
 btn.addEventListener('click', clicked)
 function clicked() {
   getColor();
+  copy.innerHTML = "POr fin"
 };
 
-   
-
 function getColor() {
-   fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${mode.value}`)
-        .then(res => res.json())
-        .then(data => {
-            const postColors = data.colors
-            render(postColors)
-        })
-  }
+  fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${mode.value}`)
+    .then(res => res.json())
+    .then(data => {
+      const postColors = data.colors
+      render(postColors)
+    })
+}
 
 function render(postColors) {
-    let html = ''
-    for (let color of postColors){
-       html += `
+  let html = ''
+  for (let color of postColors) {
+    // console.log(color.name.value)
+    html += `
          <div class="grid" style="background-color:${color.hex.value}">
-           <h3 id="text" class="text" onclick="clicked()" data-clipboard-text="${color.hex.value}"><span>${color.hex.value}</span></h3> 
+           <h3 onclick="clicked()" type="text" id="text" class="text" data-clipboard-text="${color.hex.value}"><span>${color.hex.value}</span></h3> 
          </div>
         `}
-   grid.innerHTML = html
+  grid.innerHTML = html
 }
 
 clipboard.on('success', function (e) {
-text.textContent = (`${e.action} ${e.text}`);
-    copy.innerHTML = (`data-clipboard-text="${color.hex.value}"`)
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-    setTimeout(copy.textContent = "", 6000);
-  });
+  title.innerHTML = "ITEM COPY"
+  copy.innerHTML = "Item Copy"
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+  // time()
+});
+
+function time() {
+  setTimeout(title.innerHTML = "COLOR SCHEME GENERATOR", 6000);
+}
+
 
 clipboard.on('error', function (e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-  });
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+});
 
 function randomColor() {
-    const combination = "abcdefg0123456789";
-    let result = '';
-    const combLength = combination.length;
-    for (let i = 0; i < 6; i++){
-      result += combination.charAt(Math.floor(Math.random() * combLength))
-     }
+  const combination = "abcdefg0123456789";
+  let result = '';
+  const combLength = combination.length;
+  for (let i = 0; i < 6; i++) {
+    result += combination.charAt(Math.floor(Math.random() * combLength))
+  }
   return result
 }
-  
+
 function manyColors() {
-    const colores  = randomColor();
-    colorInput.defaultValue = '#' + colores
-    body.style.backgroundColor = '#' + colores
-    getColor();
+  const colores = randomColor();
+  colorInput.defaultValue = '#' + colores
+  body.style.backgroundColor = '#' + colores
+  getColor();
 }
 
- manyColors()
- const timeOut = () => {
-   setTimeout(copy.textContent = "YAaaaa", 10000);
+manyColors()
+const timeOut = () => {
+  setTimeout(copy.textContent = "YAaaaa", 10000);
 };
 
-  
+
 
